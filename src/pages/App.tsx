e2da7e5def3688f690adf2a17003f61523f89839
innseonn <innseonn@gmail.com>
2023-03-20 23:06:13 +0900

    Feat: 하단 앱 바 추가

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,9 +1,22 @@
-import React from 'react';
+import GlobalStyle, { MaxLayout } from '../components/GlobalStyle';
+import styled from 'styled-components';
+import AppBar from '../components/AppBar';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+const AppLayout = styled.div`
+  ${MaxLayout}
+  min-height: 100vh;
+  padding: var(--padding-container);
+`;
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <BrowserRouter>
+      <GlobalStyle/>
+      <AppLayout className='App'>
+        <AppBar/>
+      </AppLayout>
+    </BrowserRouter>
   );
 }
 
