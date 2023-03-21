c8a61f783ec2745e37fd348d642f80376e581993
innseonn <innseonn@gmail.com>
2023-03-22 00:49:25 +0900

    Style: 탭 사이즈 변경

--- src/components/AppBar.tsx
+++ src/components/AppBar.tsx
@@ -4,61 +4,62 @@ import { FaBook, FaSearch, FaTrophy } from 'react-icons/fa';
 import { NavLink } from 'react-router-dom';
 
 const AppBarLayout = styled.nav`
-	position: fixed;
-	inset: auto auto 0 0;
-	width: 100%;
-	height: 80px;
-	background-color: #fff;
+  position: fixed;
+  inset: auto auto 0 0;
+  width: 100%;
+  height: 80px;
+  background-color: #fff;
 `;
 const AppBarList = styled.ul`
-	${MaxLayout}
-	display: flex;
-	justify-content: space-between;
-	align-items: center;
-	height: 100%;
-	padding: 0 var(--padding-x);
-	& > li {
-		display: flex;
-		align-items: center;
-	}
+  ${MaxLayout}
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 100%;
+  padding: 0 var(--padding-x);
+  & > li {
+    display: flex;
+    align-items: center;
+  }
 `;
 const AppBarLink = styled(NavLink)`
-	display: flex;
-	flex-direction: column;
-	justify-content: center;
-	align-items: center;
-	height: 100%;
-	padding: 0 0.4em;
-	font-size: 22px;
-	&.active {
-		color: var(--color-green);
-	}
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  padding: 0 0.4em;
+  font-size: 22px;
+  &.active {
+    color: var(--color-green);
+    
+  }
 `;
 const AppBarLinkText = styled.span`
-	margin-top: 6px;
-	font-size: 12px;
+  margin-top: 6px;
+  font-size: 12px;
 `;
 
 export default function AppBar() {
-	return (
-		<AppBarLayout>
-			<AppBarList>
-				<li>
-					<AppBarLink to='/book' className={({isActive}) => (isActive ? 'active' : '')}>
-						<FaBook/><AppBarLinkText>도감</AppBarLinkText>
-					</AppBarLink>
-				</li>
-				<li>
-					<AppBarLink to='/search' className={({isActive}) => (isActive ? 'active' : '')}>
-						<FaSearch/><AppBarLinkText>찾기</AppBarLinkText>
-					</AppBarLink>
-				</li>
-				<li>
-					<AppBarLink to='/rank' className={({isActive}) => (isActive ? 'active' : '')}>
-						<FaTrophy/><AppBarLinkText>랭킹</AppBarLinkText>
-					</AppBarLink>
-				</li>
-			</AppBarList>
-		</AppBarLayout>
-	)
+  return (
+    <AppBarLayout>
+      <AppBarList>
+        <li>
+          <AppBarLink to='/book' className={({isActive}) => (isActive ? 'active' : '')}>
+            <FaBook/><AppBarLinkText>도감</AppBarLinkText>
+          </AppBarLink>
+        </li>
+        <li>
+          <AppBarLink to='/search' className={({isActive}) => (isActive ? 'active' : '')}>
+            <FaSearch/><AppBarLinkText>찾기</AppBarLinkText>
+          </AppBarLink>
+        </li>
+        <li>
+          <AppBarLink to='/rank' className={({isActive}) => (isActive ? 'active' : '')}>
+            <FaTrophy/><AppBarLinkText>랭킹</AppBarLinkText>
+          </AppBarLink>
+        </li>
+      </AppBarList>
+    </AppBarLayout>
+  )
 }
--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -6,69 +6,69 @@ import Title from '../components/Title';
 import SubTitle from '../components/SubTitle';
 
 const SearchCardBox = styled.div`
-	display: grid;
-	grid-template-columns: 1fr 1fr;
-	grid-auto-rows: 1fr;
-	grid-gap: var(--padding-x);
-	margin-top: calc(var(--padding-x) * 2);
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-auto-rows: 1fr;
+  grid-gap: var(--padding-x);
+  margin-top: calc(var(--padding-x) * 2);
 `;
 const SearchCard = styled.article`
-	border-radius: 20px;
-	background-color: #fff;
-	box-shadow: 4px 4px 12px 0px #cdcdcd5e;
-	transition: opacity 0.5s;
-	@media screen and (any-pointer: fine) {
-		opacity: 0.6;
-		&:hover {
-			opacity: 1;
-		}
-	}
+  border-radius: 20px;
+  background-color: #fff;
+  box-shadow: 4px 4px 12px 0px #cdcdcd5e;
+  transition: opacity 0.5s;
+  @media screen and (any-pointer: fine) {
+    opacity: 0.6;
+    &:hover {
+      opacity: 1;
+    }
+  }
 `;
 const SearchCardLink = styled(Link)`
-	display: flex;
-	flex-direction: column;
-	width: 100%;
-	height: 100%;
-	padding: 24px 24px 20%;
-	& > svg {
-		color: var(--color-green);
-		font-size: var(--font-size-xl);
-	}
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+  padding: 24px 24px 20%;
+  & > svg {
+    color: var(--color-green);
+    font-size: var(--font-size-xl);
+  }
 `;
 const SearchCardText = styled.span`
-	width: 50%;
-	padding-top: 0.5em;
-	font-size: var(--font-size-l);
-	font-family: 'ELAND_Choice_B';
-	line-height: 1.4;
-	word-break: keep-all;
+  width: 50%;
+  padding-top: 0.5em;
+  font-size: var(--font-size-l);
+  font-family: 'ELAND_Choice_B';
+  line-height: 1.4;
+  word-break: keep-all;
 `;
 
 export default function Search() {
-	return (
-		<div>
-			<Title/>
-			<SubTitle text={`나와 닮은<br>동물의 숲 주민을<br>찾아보세요!`}/>
-			<SearchCardBox>
-				<SearchCard>
-					<SearchCardLink to='/search/feature'>
-						<FaRegSmileWink/>
-						<SearchCardText>나와 닮은</SearchCardText>
-					</SearchCardLink>
-				</SearchCard>
-				<SearchCard>
-					<SearchCardLink to='/search/birthday'>
-						<HiOutlineCake/>
-						<SearchCardText>생일이 같은</SearchCardText>
-					</SearchCardLink>
-				</SearchCard>
-				<SearchCard>
-					<SearchCardLink to='/search/favorite'>
-						<FaRegHeart/>
-						<SearchCardText>취향이 비슷한</SearchCardText>
-					</SearchCardLink>
-				</SearchCard>
-			</SearchCardBox>
-		</div>
-	)
+  return (
+    <div>
+      <Title/>
+      <SubTitle text={`나와 닮은<br>동물의 숲 주민을<br>찾아보세요!`}/>
+      <SearchCardBox>
+        <SearchCard>
+          <SearchCardLink to='/search/feature'>
+            <FaRegSmileWink/>
+            <SearchCardText>나와 닮은</SearchCardText>
+          </SearchCardLink>
+        </SearchCard>
+        <SearchCard>
+          <SearchCardLink to='/search/birthday'>
+            <HiOutlineCake/>
+            <SearchCardText>생일이 같은</SearchCardText>
+          </SearchCardLink>
+        </SearchCard>
+        <SearchCard>
+          <SearchCardLink to='/search/favorite'>
+            <FaRegHeart/>
+            <SearchCardText>취향이 비슷한</SearchCardText>
+          </SearchCardLink>
+        </SearchCard>
+      </SearchCardBox>
+    </div>
+  )
 }
